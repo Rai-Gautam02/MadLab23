3b849a8a80c6a4215565f174f90eb42e6f66318a
Rai-Gautam02 <saint.gautam02@gmail.com>
2023-05-16 22:29:13 +0530

    Add files via upload

---
+++ TABLE.java
@@ -0,0 +1,16 @@
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    int i,x,value=0;
+	    x=Integer.parseInt(args[0]);
+	    for(i=1;i<=10;i++)
+	    {
+	        value=x*i;
+	        System.out.println(x+"*"+i+"="+value);
+	    }
+	    
+	    
+	}
+}
---
+++ pattern.java
@@ -0,0 +1,20 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		int i=0,j=0,k=0;
+		for(i=0;i<=4;i++)
+		{
+		    for(k=4;k>=i;k--)
+		    {
+		        System.out.print(" ");
+		    }
+		    for(j=0;j<i;j++)
+		    {
+		        System.out.print("* ");
+		    }
+		   System.out.println("");
+		    
+		}
+	}
+}
